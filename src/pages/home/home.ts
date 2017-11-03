2ce129b48acc89605ede00dddfa11666caf1a565
maramalquraan <quraanmaram@gmail.com>
2017-11-03 17:56:19 -0400

    nani location on map

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,18 +6,18 @@ import { AngularFireDatabase } from "angularfire2/database";
 import firebase from 'firebase';
 import { ActionSheetController } from 'ionic-angular';
 import { PricePage } from '../price/price';
+import{Geolocation} from '@ionic-native/geolocation';
 import {
 	GoogleMaps,
-	GoogleMap,
-	CameraPosition,
+  GoogleMap,
+  CameraPosition,
 	LatLng,
-	GoogleMapsEvent,
-	Marker,
-  MarkerOptions
+	GoogleMapsEvent
 } from '@ionic-native/google-maps';
-import{Geolocation} from '@ionic-native/geolocation';
+
 
 declare var google: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -28,6 +28,7 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: GoogleMap;
   nani;
+  coordinates={};
   public toggleStatus: boolean;
   constructor(private afAuth : AngularFireAuth,
     public navCtrl: NavController,
@@ -42,41 +43,54 @@ export class HomePage {
    });
   }
   
+  userPosition;
+
   logout(){
   this.afAuth.auth.signOut()  
   this.navCtrl.setRoot(LoginPage) 
   }
+
+  moveCamera(loc: LatLng){
+    let options: CameraPosition<LatLng> = {
+      target: loc,
+      zoom: 15,
+      tilt: 10
+
+    }
+    this.map.moveCamera(options)
+  }
+
+
   ngAfterViewInit(){
+  let loc: LatLng;
   this.initMap();
-  this.getLocation();
+
+  this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+    this.getLocation().then(res => {
+      loc= new LatLng(res.coords.latitude, res.coords.longitude);
+      this.moveCamera(loc);
+    }).catch(err => {
+      console.log(err);
+    });
+  });
+  }
   
 
-  }
+  
 
   ionViewWillLoad(){
 
   }
+
+
   initMap(){
     let element = this.mapElement.nativeElement;
-    this._geoLoc.getCurrentPosition().then((position)=> {
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      console.log("nani long and lat",lng,lat)
-  })
-
     this.map = this._googleMaps.create(element);
   }  
 
-  getLocation(){
-    this._geoLoc.getCurrentPosition().then((position)=> {
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-        console.log("nani long and lat",lng,lat)
-    })
-
-  }
-  
-
+getLocation(){
+return this._geoLoc.getCurrentPosition();
+}
 
   trackNani(){
     console.log("initial toggle state", this.toggleStatus )
@@ -109,8 +123,101 @@ export class HomePage {
     }
    
    }
-  timer(){
-    this.navCtrl.push(PricePage);
+  
+
+   isRequested(){
+     //if nani is requested
+     
+   }
+   showDirectionAndDuration(){
+    //direction code
+    let x = this;
+    let markerArray = [];
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer({map: this.map});
+    let stepDisplay = new google.maps.InfoWindow;
+    this.calculateAndDisplayRoute( directionsDisplay, directionsService, markerArray, stepDisplay, this.map);
+    var onChangeHandler = function() {
+      x.calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, this.map);
+    };
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    // document.getElementById('end').addEventListener('change', onChangeHandler);
+    //duration code
+    
+    // var bounds = new google.maps.LatLngBounds;
+    // var destination = 'Yaser Mall';
+    // var origin = 'Mecca Mall';
+    // var origin = {lat: 31.977285, lng: 35.843623};
+    // var destination = {lat: 31.955330, lng: 35.834616};
+    var origin = this.coordinates;
+    var destination = x.userPosition;
+    // var geocoder = new google.maps.Geocoder;
+    var service = new google.maps.DistanceMatrixService;
+    service.getDistanceMatrix({
+      origins: [origin],
+      destinations: [destination],
+      travelMode: 'DRIVING',
+      unitSystem: google.maps.UnitSystem.METRIC,
+      avoidHighways: false,
+      avoidTolls: false
+    },function(response, status) {
+        if (status !== 'OK') {
+          alert('Error was: ' + status);
+        } else {
+            var originList = response.originAddresses;
+            var destinationList = response.destinationAddresses;
+            var outputDiv = document.getElementById('output');
+            outputDiv.innerHTML = '';
+            for (var i = 0; i < originList.length; i++) {
+              var results = response.rows[i].elements;
+              console.log(results)
+              for (var j = 0; j < results.length; j++) {
+              outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                ': ' + results[j].distance.text + ' in ' +
+                results[j].duration.text + '<br>';
+              }
+            }
+          }
+      });
+  }
+  calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map){
+    let x = this;
+    for (var i = 0; i < markerArray.length; i++) {
+      markerArray[i].setMap(null);
+    }
+    directionsService.route({
+      origin: this.coordinates,
+      destination: x.userPosition,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      // Route the directions and pass the response to a function to create
+      // markers for each step.
+      if (status === 'OK') {
+        document.getElementById('warnings-panel').innerHTML =
+            '<b>' + response.routes[0].warnings + '</b>';
+        directionsDisplay.setDirections(response);
+        x.showSteps(response, markerArray, stepDisplay, map);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+  showSteps(directionResult, markerArray, stepDisplay, map){
+    var myRoute = directionResult.routes[0].legs[0];
+    for (var i = 0; i < myRoute.steps.length; i++) {
+      var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+      marker.setMap(map);
+      marker.setPosition(myRoute.steps[i].start_location);
+      this.attachInstructionText(stepDisplay, marker, myRoute.steps[i].instructions, map);
+    }
+  }
+  attachInstructionText(stepDisplay, marker, text, map){
+    google.maps.event.addListener(marker, 'click', function() {
+      // Open an info window when the marker is clicked on, containing the text
+      // of the step.
+      stepDisplay.setContent(text);
+      stepDisplay.open(map, marker);
+    });
   }
 }
 
