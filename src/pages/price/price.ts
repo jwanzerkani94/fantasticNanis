a00821f3c14e1632d3d129434736d48ea7f032c0
Maram Al-Quraan <30745854+maramalquraan@users.noreply.github.com>
2017-11-05 03:22:34 -0500

    Update price.ts

--- src/pages/price/price.ts
+++ src/pages/price/price.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import firebase from "firebase";
 import { AngularFireDatabase } from "angularfire2/database";
+import { AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the PricePage page.
@@ -16,49 +18,73 @@ import { AngularFireDatabase } from "angularfire2/database";
   templateUrl: 'price.html',
 })
 export class PricePage {
-  information ={
-    nani_id : "YdSV2gxkYoO84TtnOoOjBauEJB33" ,
-    user_id : "7dfXMMQQsfN7MqpHHdYF3EBwSFX2" ,
-    start_time: "7:43:16 PM"
-  }
+  service;
+  nanis;
+  users;
+  money;
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public db: AngularFireDatabase) {
-    let database = firebase.database();
+              public db: AngularFireDatabase,
+              public alertCtrl: AlertController) {
     db.object("services").valueChanges().subscribe(data => {
       this.service = data;
     });
+    let database = firebase.database();
+    db.object("nani").valueChanges().subscribe(data => {
+        this.nanis = data;
+    });
+    db.object("users").valueChanges().subscribe(data => {
+      this.users = data;
+    });
   }
-  service;
+
   // ionViewOnLoad(){
   //   this.showInfo();
   // }
-
   showInfo(){
+    let service_id;
+    let user_id;
+    let nani_id;
+    for(var key in this.service){
+      //this needs to be solved when we connect the apps
+      if(key === '6288119'){
+        service_id = key;
+        nani_id = this.service[key].nani_id;
+        user_id = this.service[key].user_id;
+      }
+    }
     var naniName = document.getElementById("naniName");
     var userName = document.getElementById("userName");
     var startTime = document.getElementById("time");
     naniName.innerHTML = "";
     userName.innerHTML = "";
     startTime.innerHTML = "";
-    naniName.innerHTML += this.information['nani_id'];
-    userName.innerHTML += this.information['user_id'];
-    startTime.innerHTML += this.information['start_time'];
+    naniName.innerHTML += this.nanis[nani_id].firstName +" "+ this.nanis[nani_id].lastName;
+    userName.innerHTML += this.users[user_id].firstName +" "+ this.users[user_id].lastName;
+    startTime.innerHTML += this.service[service_id].start_time;
   }
 
   calculatePrice(){
     this.stopTimer();
     let db = firebase.database();
     var currentTime = new Date(new Date().getTime()).toLocaleTimeString();
-    db.ref("services/4160189").update({ end_time : currentTime });
-    this.timeToMoney(this.service[4160189].start_time,currentTime);
+    //also this line 
+    db.ref("services/6288119").update({ end_time : currentTime });
+    this.timeToMoney(this.service[6288119].start_time,currentTime);
   }
 
   timeToMoney(start , end) {
-    console.log(start,end)
+    let x = this;
     var startTime = start.split(":")
     var endTime = end.split(":")
-    var money = (Number(endTime[0])*60 + Number(endTime[1]) - Number(startTime[0])*60 - Number(startTime[1])) * (20/60);
-    alert("total price : " + money) 
+    this.money = (Number(endTime[0])*60 + Number(endTime[1]) - Number(startTime[0])*60 - Number(startTime[1])) * (20/60);
+    // alert("total price : " + money) 
+    let alert = this.alertCtrl.create({
+      title: 'Total Price :',
+      subTitle: x.money,
+      buttons: ['OK']
+    });
+    alert.present();
+    this.navCtrl.push(HomePage);
   }
 
   seconds = 0;
@@ -90,9 +116,11 @@ export class PricePage {
   
 
   ionViewDidLoad() {
+    let x = this;
     console.log('ionViewDidLoad PricePage');
-    this.showInfo();
+    var timeOut = setTimeout(function(){
+      x.showInfo()
+    },200);
     this.startTimer();
   }
-
 }
